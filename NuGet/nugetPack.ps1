cd003dfb9a84101d729c34f1419050d449979faf
Anatoliy Koperin <a.koperin@abbyy-ls.com>
2015-01-16 17:10:19 +0300

    configure NuGet deploy

---
+++ NuGet/nugetPack.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = "Stop"
+$mainFolder = Resolve-Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+$nugetExe = "$mainFolder\nuget.exe"
+
+Remove-Item $mainFolder\*.nupkg
+& "$nugetExe" pack $mainFolder/../Payments/Payments.csproj -Properties Configuration=Release
